39967d8ebd2a49c29bd43948f9e80a67488c9f4d
sherzod <azamovshavkat98@gmail.com>
2024-06-09 18:49:16 +0500

    celery task corrected, datas got from api

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 import os
-from Bitrix.formatting import format_bitrix_data
+import requests
 from celery import Celery
 from celery.schedules import crontab
 
@@ -22,6 +22,7 @@ app.conf.beat_schedule = {
 }
 
 SHEET_URL = os.environ.get('SHEET_URL')
+API_URL = os.environ.get('API_URL')
 
 google_obj = GoogleSheetsManager(
     credentials_file='credentials.json',
@@ -31,17 +32,20 @@ google_obj = GoogleSheetsManager(
 
 @app.task()
 def add_every_night():
-    bitrix_datas = format_bitrix_data()
-    for data in bitrix_datas:
-        call_data = CallData(
-            date_str=data['date'],
-            employee=data['seller'],
-            phone_number=data['client_phone_number'],
-            client=data['client'],
-            call_status=data['direction'],
-            answered=data['answered'],
-            call_duration=data['duration'],
-            dialing=data['dialing'],
-            source='Номер не указан'
-        )
-        google_obj.append_row(call_data.format_data(len(google_obj.get_sheet_data()) + 1))
+    url = f'{API_URL}/data-google-sheet'
+    r = requests.get(url)
+    if r.json()['detail'] != 'There are some problems with Bitrix, please try again later!':
+        bitrix_datas = r.json()['data']
+        for data in bitrix_datas:
+            call_data = CallData(
+                date_str=data['date'],
+                employee=data['seller'],
+                phone_number=data['client_phone_number'],
+                client=data['client'],
+                call_status=data['direction'],
+                answered=data['answered'],
+                call_duration=data['duration'],
+                dialing=data['dialing'],
+                source='Номер не указан'
+            )
+            google_obj.append_row(call_data.format_data(len(google_obj.get_sheet_data()) + 1))
