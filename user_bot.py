05e84d90fd3292a97b560f2a7cf4384851e19978
sherzod <azamovshavkat98@gmail.com>
2024-06-12 11:07:39 +0500

    running of docker-compose correcte

--- user_bot.py
+++ user_bot.py
@@ -10,37 +10,42 @@ api_hash = os.environ.get('API_HASH')
 with TelegramClient('name', api_id, api_hash) as client:
     @client.on(events.NewMessage(pattern='(?i).*Assalomu Alaykum'))
     async def handler(event):
-        await event.reply(
-            "Va Alaykum Assalom, Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
-        await event.reply('Rahmat 😊')
+        if event.is_private:
+            await event.reply(
+                "Va Alaykum Assalom, Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
+            await event.reply('Rahmat 😊')
 
 
     @client.on(events.NewMessage(pattern='(?i).*qalesan'))
     async def handler(event):
-        await event.reply(
-            "Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
-        await event.reply('Rahmat 😊')
+        if event.is_private:
+            await event.reply(
+                "Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
+            await event.reply('Rahmat 😊')
 
 
     @client.on(events.NewMessage(pattern='(?i).*qalesiz'))
     async def handler(event):
-        await event.reply(
-            "Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
-        await event.reply('Rahmat 😊')
+        if event.is_private:
+            await event.reply(
+                "Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
+            await event.reply('Rahmat 😊')
 
 
     @client.on(events.NewMessage(pattern='(?i).*sher'))
     async def handler(event):
-        await event.reply(
-            "Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
-        await event.reply('Rahmat 😊')
+        if event.is_private:
+            await event.reply(
+                "Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
+            await event.reply('Rahmat 😊')
 
 
     @client.on(events.NewMessage(pattern='(?i).*sherzod'))
     async def handler(event):
-        await event.reply(
-            "Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
-        await event.reply('Rahmat 😊')
+        if event.is_private:
+            await event.reply(
+                "Assalomu Alaykum, men Sherzodning yordamchisiman, Sherzod hozir dam olmoqda yoki ishda, iltimos, biror savol bo'lsa yozib qoldiring!")
+            await event.reply('Rahmat 😊')
 
 
     client.run_until_disconnected()
