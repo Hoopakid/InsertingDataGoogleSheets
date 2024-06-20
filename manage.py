141d50e8a699ed9388fa9559f5fd3ad5940f2b51
Sherzod <azamovshavkat98@gmail.com>
2024-06-20 16:17:18 +0500

    apis and scrapping files are corrected

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import logging
 from celery import Celery
 from celery.schedules import crontab
 
-from setup import GoogleSheetsManager, CallData
+from setup import GoogleSheetsManager
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,23 +24,25 @@ app.conf.beat_schedule = {
     'add_bulut': {
         'task': 'manage.add_bulut',
         'schedule': crontab(hour=22, minute=0)
+    },
+    'add_bulut1': {
+        'task': 'manage.add_bulut1',
+        'schedule': crontab(hour=18, minute=0)
     }
 }
 
 SHEET_URL = os.environ.get('SHEET_URL')
 API_URL = os.environ.get('API_URL')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+USER_CHAT_ID = os.environ.get('USER_CHAT_ID')
 
-margarit_google_obj = GoogleSheetsManager(
-    credentials_file='credentials.json',
-    sheet_url=SHEET_URL,
-    worksheet_name='Margaritto Yangi akab'
-)
 
-bulut_google_obj = GoogleSheetsManager(
-    credentials_file='credentials.json',
-    sheet_url=SHEET_URL,
-    worksheet_name='Bulut Yangi AKB'
-)
+def get_obj_sheet(worksheet_name):
+    return GoogleSheetsManager(
+        credentials_file='credentials.json',
+        sheet_url=SHEET_URL,
+        worksheet_name=worksheet_name
+    )
 
 
 @app.task()
@@ -48,6 +50,7 @@ def add_margarit():
     bulut_margarit_akb_url = f'{API_URL}/get-akb-data'
     r = requests.get(bulut_margarit_akb_url)
     logging.info('Data fetched')
+    margarit_google_obj = get_obj_sheet('Margaritto Yangi akab')
     if r.json()['status'] == 200:
         margarit_data = r.json()['data']['Margarit']
         for data in margarit_data:
@@ -56,6 +59,10 @@ def add_margarit():
                 temp.append(val)
             margarit_google_obj.append_row(temp)
         logging.info('Margarit data added')
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}'
+    requests.post(url + '/sendMessage',
+                  data={'chat_id': USER_CHAT_ID, 'text': 'Margaritto data added to Google Sheets'})
+    return True
 
 
 @app.task()
@@ -63,7 +70,8 @@ def add_bulut():
     bulut_margarit_akb_url = f'{API_URL}/get-akb-data'
     r = requests.get(bulut_margarit_akb_url)
     logging.info('Data fetched')
-    if r.json()['status'] == 200:
+    bulut_google_obj = get_obj_sheet('Bulut Yangi AKB')
+    if r.status_code == 200:
         bulut_data = r.json()['data']['Bulut']
         for data in bulut_data:
             temp = []
@@ -71,3 +79,27 @@ def add_bulut():
                 temp.append(val)
             bulut_google_obj.append_row(temp)
         logging.info('Bulut data added')
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}'
+    requests.post(url + '/sendMessage',
+                  data={'chat_id': USER_CHAT_ID, 'text': 'Bulut data added to Google Sheets'})
+    return True
+
+
+@app.task()
+def add_bulut1():
+    bulut_margarit_akb_url = f'{API_URL}/get-all-data'
+    r = requests.get(bulut_margarit_akb_url)
+    logging.info('Data fetched')
+    google_obj = get_obj_sheet('Bulut/Margaritto')
+    if r.status_code == 200:
+        datas = r.json()
+        for data in datas:
+            temp = []
+            for key, val in data.items():
+                temp.append(val)
+            google_obj.append_row(temp)
+        logging.info('Bulut data added')
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}'
+    requests.post(url + '/sendMessage',
+                  data={'chat_id': USER_CHAT_ID, 'text': 'Bulut/Margaritto data added to Google Sheets'})
+    return True
